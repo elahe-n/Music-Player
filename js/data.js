c105c2f14dd8eb90a31415c2da785c25fb11381f
Elahe <nourkami.e@gmail.com>
2021-12-18 14:14:45 -0500

    Separate song's data and utilities in other modules

---
+++ js/data.js
@@ -0,0 +1,123 @@
+// Song's data
+export const playlist = [
+  {
+  title:'Bohemian Rhapsody',
+  singer:'Freddie Mercury',
+  year:'1975',
+  duration:'',
+  album:'A Night at the Opera',
+  genre:'Hard rock',
+  lyrics:``,
+  pic:`1.jfif`,
+  url:'https://cdns-preview-1.dzcdn.net/stream/c-17597947a0fdd6e8ea971f146755cd34-13.mp3',
+  favorite:false
+},
+{
+  title:'We Will Rock You',
+  singer:'Freddie Mercury',
+  year:'1977',
+  duration:'',
+  album:'Live Killers',
+  genre:'Hard rock',
+  lyrics:'',
+  pic:`2.jfif`,
+  url:'https://www.naijagreen.com/wp-content/uploads/music/2021/09/Queen_-_We_Will_Rock_You_[NaijaGreen.Com]_.mp3',
+  favorite:false
+},
+{
+  title:'We Are The Champions',
+  singer:'Freddie Mercury',
+  year:'1979',
+  duration:'',
+  album:'Live Killers',
+  genre:'Arena rock',
+  lyrics:'',
+  pic:`3.jfif`,
+  url:'https://cdns-preview-7.dzcdn.net/stream/c-7db6af13d4ef830810c43bc28bf6f779-13.mp3',
+  favorite:false
+},
+{
+  title:'Crazy Little Thing Called Love',
+  singer:'Freddie Mercury',
+  year:'1979',
+  duration:'',
+  album:'The Game',
+  genre:'Rockabilly',
+  lyrics:'',
+  pic:`4.jfif`,
+  url:'https://cdns-preview-c.dzcdn.net/stream/c-ce852e942a9d52502ddc0f2922bdc97c-8.mp3',
+  favorite:false
+},
+{
+  title:'Dont Stop Me Now',
+  singer:'Freddie Mercury',
+  year:'1979',
+  duration:'',
+  album:'Jazz',
+  genre:'Pop rock',
+  lyrics:'',
+  pic:`5.jfif`,
+  url:'https://cdns-preview-c.dzcdn.net/stream/c-c62114bb9ed46619356f9aee5a8d4102-6.mp3',
+  favorite:false
+},
+{
+  title:'Somebody To Love',
+  singer:'Freddie Mercury',
+  year:'1976',
+  duration:'',
+  album:'A Day at the Races',
+  genre:'Rock',
+  lyrics:'',
+  pic:`6.jfif`,
+  url:'https://cdns-preview-9.dzcdn.net/stream/c-9e208326d4114be07cbadc20ee8394c8-8.mp3',
+  favorite:false
+},
+{
+  title:'Youre My Best Friend',
+  singer:'Freddie Mercury',
+  year:'1975',
+  duration:'',
+  album:'A Night at the Opera',
+  genre:'Rock',
+  lyrics:'',
+  pic:`7.jfif`,
+  url:'https://cdns-preview-d.dzcdn.net/stream/c-dec1e38df08f51a46f67e49b0dbcc29a-6.mp3',
+  favorite:false
+},
+{
+  title:'Radio Ga Ga',
+  singer:'Freddie Mercury',
+  year:'1984',
+  duration:'',
+  album:'The Works',
+  genre:'POP Rock',
+  lyrics:'',
+  pic:`8.jfif`,
+  url:'https://www.naijagreen.com/wp-content/uploads/music/2021/08/Queen_-_Radio_Ga_Ga_[NaijaGreen.Com]_.mp3',
+  favorite:false
+},
+{
+  title:'Too Much Love Will Kill You',
+  singer:'Freddie Mercury',
+  year:'1992',
+  duration:'',
+  album:'Back to the Light',
+  genre:'Rock',
+  lyrics:'',
+  pic:`9.jfif`,
+  url:'https://cdns-preview-2.dzcdn.net/stream/c-23cadb95a9707bb26c7c69b6297c180d-6.mp3',
+  favorite:false
+},
+{
+  title:`Dont try so hard`,
+  singer:'Freddie Mercury',
+  year:'1991',
+  duration:'',
+  album:'Innuendo',
+  genre:'Rock',
+  lyrics:'',
+  pic:`10.jfif`,
+  url:'https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/Music3/v4/9a/cb/e4/9acbe4b9-1f0e-903e-89f7-2d6ba91be31b/mzaf_7265951792807668176.plus.aac.p.m4a',
+  favorite:false
+}
+]
